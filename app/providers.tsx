25206631c0dc76b9e52d2f51430be5d8145df00f
John Kristan Torremocha <torremocha.johnkristan@gmailo.com>
2026-02-14 15:08:38 +0800

    refactor: change environment variables

--- app/providers.tsx
+++ app/providers.tsx
@@ -6,8 +6,8 @@ import { useEffect } from 'react'
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
     useEffect(() => {
-        const key = process.env.NEXT_PUBLIC_POSTHOG_KEY
-        const host = process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com'
+        const key = process.env.POSTHOG_KEY
+        const host = process.env.POSTHOG_HOST || 'https://us.i.posthog.com'
 
         if (key) {
             posthog.init(key, {
